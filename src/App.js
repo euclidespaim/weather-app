5f06b6fdd6f697d64d3875928a106b414fcd9829
euclidespaim <euclidespaim@gmail.com>
2020-05-15 15:04:13 -0300

    Weather-app initial files...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+const api = {
+  key: "a2c75f45d6361c6b98c8df80af839c77",
+  base: "https://api.openweathermap.org/data/2.5"
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
